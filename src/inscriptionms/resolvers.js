d81dd284cd01eaedceeadbe1b5cce2b415ae599e
ncontrerasn <ncontrerasn@unal.edu.co>
2020-10-07 18:45:25 -0500

    a

--- src/inscriptionms/resolvers.js
+++ src/inscriptionms/resolvers.js
@@ -23,7 +23,7 @@ const resolvers = {
             return response;
         },
         deleteInscription: (_, { id }) =>{
-            let response = deleteDeleteInscription(`${URL}/${id}`);
+            let response = deleteDeleteInscription(`${URL}${id}`);
             console.log(URL);
             return response;
         }
