71b4d4c23dc4176736cd9ef6821dfe6008a34a1d
ncontrerasn <ncontrerasn@unal.edu.co>
2020-10-07 18:17:11 -0500

    a

--- src/inscriptionms/server.js
+++ src/inscriptionms/server.js
@@ -1,4 +1,4 @@
 export const url = '18.209.220.131'
 export const port = '7070'
-export const entryPoint = '/inscriptions'
+export const entryPoint = '/inscriptions/'
 export const entryPoint_user = '/userInscription'
