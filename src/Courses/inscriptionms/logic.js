f2279f4fd1ec35894dd9c7dc6844d8ca75b410bd
ncontrerasn <ncontrerasn@unal.edu.co>
2020-10-18 21:35:29 -0500

    a

--- src/Courses/inscriptionms/logic.js
+++ src/Courses/inscriptionms/logic.js
@@ -9,6 +9,7 @@ export async function getInscriptionByUserId(url_inscription_ms){
 	for(var y in res.data){
 		var id = res.data[y].id_curso;
 		let res2 = await infoResolvers.Query.getCourseName(null, {id:{entero:id}});
+		console.log(id);
 		console.log(res2);
 		nombres[y] = {"id" : res2, "course": res2};
 	}
