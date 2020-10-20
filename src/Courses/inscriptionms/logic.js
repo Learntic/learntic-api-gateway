065b366c9bcb5033c426d4e1a2affb5bbedc8596
ncontrerasn <ncontrerasn@unal.edu.co>
2020-10-19 22:57:38 -0500

    a

--- src/Courses/inscriptionms/logic.js
+++ src/Courses/inscriptionms/logic.js
@@ -19,6 +19,7 @@ export async function coursesByNotUserId(url_inscription_ms){
 	let res = await axios.get(url_inscription_ms);
 	var cursos = [];
 	let res3 = await infoResolvers.Query.getCoursesId(null);
+	console.log(res3[0]);
 	for(var x in res3.data){
 		console.log("for1");
 		if(res.data.indexOf(res3.data[x]) != -1)
