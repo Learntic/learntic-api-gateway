dc3a7cb36433a289d199610a41e23b7c437e4606
ncontrerasn <ncontrerasn@unal.edu.co>
2020-10-20 00:29:08 -0500

    a

--- src/Courses/infoms/logic.js
+++ src/Courses/infoms/logic.js
@@ -79,7 +79,7 @@ export async function getAllCourses(url_info_ms){
 	let res = await axios.get(url_info_ms);
 	console.log(res.data);
 	for(var y in res.data){
-		var id = res[y].id_curso;
+		var id = res.data[y].id_curso;
 		let puntaje = await feedbackResolvers.Query.feedbackScore(null, {id_curso:id});
 		res[y].course_score = puntaje;
 	}
