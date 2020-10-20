7210cf5b01634b424e6f7ab60024311eeaa78114
ncontrerasn <ncontrerasn@unal.edu.co>
2020-10-19 23:29:17 -0500

    a

--- src/Courses/inscriptionms/logic.js
+++ src/Courses/inscriptionms/logic.js
@@ -10,7 +10,7 @@ export async function coursesByUserId(url_inscription_ms){
 	for(var y in res.data){
 		var id = res.data[y].id_curso;
 		let res2 = await infoResolvers.Mutation.getCourse(null, {id:{entero:id}});
-		let puntaje = await feedbackResolvers.Query.feedbackScore(null, id_curso=id);
+		let puntaje = await feedbackResolvers.Query.feedbackScore(null, {id_curso:id});
 		console.log(puntaje);
 		//res2.data.course_score = puntaje;
 		cursos[y] = res2;
